9d074f995139caf5df73ec7b62ab319efd92a27a
kalxr <kevin.a.mcafee@gmail.com>
2022-03-12 23:38:57 -0600

    fasfd

--- scan.py
+++ scan.py
@@ -148,7 +148,7 @@ def scan_tls_versions(info):
 
       result = subprocess.check_output(["echo", "|", "openssl", "s_client", "tls1_3", "-connect", host+":"+str(443)],
           timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-      if error not in result:
+      if "error" not in result:
         info[host]["tls_versions"].append("TLSv1.3")
 
     except FileNotFoundError:
